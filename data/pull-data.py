435a287e66bcedc4673720717979712413acd33c
StealWonders <6114858+StealWonders@users.noreply.github.com>
2023-05-16 22:14:46 +0200

    refactor: Make code better

--- data/pull-data.py
+++ data/pull-data.py
@@ -1,17 +1,18 @@
 import os
+import sqlite3
 import zipfile
 from ftplib import FTP
 
 import pandas
-import sqlite3
-
 import requests
 import sqlalchemy
 from rich.progress import track
 
 from logger import log
 
-raw_dir: str = "raw_data"
+raw_data_dir: str = "raw_data"
+processed_data_dir: str = "processed_data"
+
 db_connection_uri: str = "sqlite:///processed_data/data.sqlite"
 # db_connection_uri: str = "sqlite:///../data.sqlite"
 engine = sqlalchemy.create_engine(db_connection_uri)
@@ -19,7 +20,17 @@ engine = sqlalchemy.create_engine(db_connection_uri)
 
 def main():
     log("Starting data collector", "info")
+
+    # Create a raw_data folder if it doesn't exist
+    if not os.path.exists(raw_data_dir):
+        os.makedirs(raw_data_dir)
+
+    # Create a processed_data folder if it doesn't exist
+    if not os.path.exists(processed_data_dir):
+        os.makedirs(processed_data_dir)
+
     pull_power_data()
+    pull_station_date()
     pull_weather_data()
     log("Completed data collection", timestamp=True)
 
@@ -27,22 +38,16 @@ def main():
 def pull_power_data():
     power_data_src: str = "https://data.open-power-system-data.org/time_series/2020-10-06/time_series.sqlite"
 
-    # Create a raw_data folder if it doesn't exist
-    if not os.path.exists("raw_data"):
-        os.makedirs("raw_data")
-
-    # Check if power_data.sqlite exists
-    if not os.path.exists("raw_data/power_data.sqlite"):
-
-        # Download time_series database for usage in pandas dataframe
+    # Only download the power_data if it doesn't exist
+    data_src_path: str = os.path.join(raw_data_dir, "power_data.sqlite")
+    if not os.path.exists(data_src_path):
         response = requests.get(power_data_src)
         if response.status_code == 200:
-            with open("raw_data/power_data.sqlite", "wb") as file:
+            with open(data_src_path, "wb") as file:
                 file.write(response.content)
-            log("Downloaded power_data", "success")
+                log("Downloaded power_data", "success")
         else:
             log("Could not download power_data", "error")
-
     else:
         log("Found power_data files (skipping download)", "success")
 
@@ -94,18 +99,15 @@ def pull_power_data():
 
     query: str = 'SELECT {} FROM time_series_60min_singleindex'.format(", ".join(columns))
 
-    connection = sqlite3.connect("raw_data/power_data.sqlite")
+    # Query our data and save it to a dataframe
+    connection = sqlite3.connect(data_src_path)
     data_frame = pandas.read_sql_query(query, connection)
     connection.close()
 
     # Debug print
     # print(data_frame)
 
-    # Create a processed_data folder if it doesn't exist
-    if not os.path.exists("processed_data"):
-        os.makedirs("processed_data")
-
-    # Save data to a sqlite database
+    # Save the data to a sqlite database if the table doesn't already exist
     if sqlalchemy.inspect(engine).has_table("power_data"):
         log(f"Found power_data table (skipping extraction)", "success")
     else:
@@ -113,13 +115,40 @@ def pull_power_data():
         log("Extracted power_data", "success")
 
 
+def pull_station_date():
+    station_data_src: str = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/subdaily/wind/historical/FK_Terminwerte_Beschreibung_Stationen.txt"
+
+    # Only download the station_data if it doesn't exist
+    data_src_path: str = os.path.join(raw_data_dir, "station_data.txt")
+    if not os.path.exists(data_src_path):
+        response = requests.get(station_data_src)
+        if response.status_code == 200:
+            with open(data_src_path, "wb") as file:
+                file.write(response.content.decode("ISO-8859-1").encode("UTF-8"))  # Convert to UTF-8 encoding
+                log("Downloaded station_data", "success")
+        else:
+            log("Could not download station_data", "error")
+    else:
+        log("Found station_data files (skipping download)", "success")
+
+    # Insert the data into the database only if the table doesn't already exist
+    if sqlalchemy.inspect(engine).has_table("station_data"):
+        log("Found station_data table (skipping extraction)", "success")
+        return
+
+    # Extract the data from the file
+    column_widths = [(0, 5), (6, 14), (15, 23), (24, 38), (39, 50), (51, 60), (61, 101), (102, None)]
+    column_names = ["station_id", "von_datum", "bis_datum", "station_height", "latitude", "longitude", "station_name", "state"]
+
+    # Read the fixed-width file into a DataFrame
+    data_frame = pandas.read_fwf(data_src_path, colspecs=column_widths, names=column_names, skiprows=2)
+    data_frame.to_sql("station_data", engine, if_exists="replace", index=False)
+
+    log("Extracted station_data", "success")
+
+
 def pull_weather_data():
     data_sources = [
-        {
-            "name": "station_data",
-            "path": "climate_environment/CDC/help/RR_Tageswerte_Beschreibung_Stationen.txt",
-            "columns": ["Stations_id", "geoBreite", "geoLaenge"]
-        },
         {
             "name": "rain_data",
             "path": "climate_environment/CDC/observations_germany/climate/daily/more_precip/historical/",
@@ -144,7 +173,7 @@ def pull_weather_data():
     ftp_uri: str = "opendata.dwd.de"
 
     for data_src in data_sources:
-        if os.path.exists(os.path.join(raw_dir, data_src['name'])):
+        if os.path.exists(os.path.join(raw_data_dir, data_src['name'])):
             log(f"Found {data_src['name']} files (skipping download)", "success")
         else:
             download(ftp_uri, data_src['name'], data_src['path'])
@@ -156,30 +185,27 @@ def pull_weather_data():
             extract_data_source(data_src['name'], data_src["columns"])
             log(f"Extracted {data_src['name']}", "success")
 
-    log(f"Pipeline completed", timestamp=True)
-
 
 def download(ftp_uri: str, data_src_name: str, path: str):
-    # Connect to FTP server and navigate to the target directory
     ftp = FTP(ftp_uri)
     ftp.login()
+
     folder_path, file_name = os.path.split(path)
     ftp.cwd(folder_path)
 
     # Get a list of all files
-    files: list[str] = []
     if file_name is None or file_name == "":
         files = ftp.nlst()
     else:
         files = [file_name]
-    raw_data_directory: str = os.path.join(raw_dir, data_src_name)
-    os.makedirs(raw_data_directory, exist_ok=True)
+
+    # todo: filter files only within the last x years
 
     # Download each file
-    desc = log(f"Downloading {data_src_name} from server ", "status", ret_str=True)
+    desc: str = log(f"Downloading {data_src_name} from server", "status", ret_str=True)
     progress = track(files, description=desc, transient=True)
     for file in progress:
-        local_filename: str = os.path.join(raw_data_directory, file)
+        local_filename: str = os.path.join(raw_data_dir, file)
         with open(local_filename, "wb") as f:
             def callback(chunk):
                 f.write(chunk)
@@ -191,20 +217,13 @@ def download(ftp_uri: str, data_src_name: str, path: str):
 
 
 def extract_data_source(data_src_name: str, filter_items: list[str]):
-    path: str = os.path.join(raw_dir, data_src_name)
-
-    # # Check if folder contains single file (e.g. station_data)
-    # files = os.listdir(path)
-    # if len(files) == 1:
-    #     with open(os.path.join(path, files[0]), "r") as file:
-    #         insert_into_db(file, data_src_name, filter_items)
-    #     return
+    path: str = os.path.join(raw_data_dir, data_src_name)
 
     # Get a list of all zip files in the folder
     zip_files = [file for file in os.listdir(path) if file.endswith(".zip")]
 
     # Iterate over the zips with a progress bar
-    desc = log(f"Extracting {data_src_name} into database ", "status", ret_str=True)
+    desc = log(f"Extracting {data_src_name} into database", "status", ret_str=True)
 
     for zip_file in track(zip_files, description=desc, transient=True):
         zip_path: str = os.path.join(path, zip_file)
@@ -213,22 +232,18 @@ def extract_data_source(data_src_name: str, filter_items: list[str]):
             # Get a list of member files contained in the zip file (excluding metadata files)
             member_files = [member for member in zip_ref.namelist() if not member.startswith("Metadaten_")]
 
+            # Read each member file into a pandas DataFrame
             for member in member_files:
-                # Read each member file into a pandas DataFrame
                 with zip_ref.open(name=member, mode="r") as tmp_file:
-                    insert_into_db(tmp_file, data_src_name, filter_items)
+                    data_frame = pandas.read_csv(tmp_file, sep=";")
 
+                    # Remove unnecessary columns from the DataFrame
+                    data_frame = data_frame.filter(items=filter_items)
 
-def insert_into_db(tmp_file, data_src_name, filter_items):
-    data_frame = pandas.read_csv(tmp_file, sep=";")
+                    # print(data_frame)
 
-    # Remove unnecessary columns from the DataFrame
-    data_frame = data_frame.filter(items=filter_items)
-
-    # print(data_frame)
-
-    # Store the data into the SQLiteDB
-    data_frame.to_sql(data_src_name, engine, if_exists="replace", index=False)
+                    # Store the data into the SQLiteDB
+                    data_frame.to_sql(data_src_name, engine, if_exists="replace", index=False)
 
 
 if __name__ == "__main__":

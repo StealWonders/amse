36f79f6508d005b45cf2bc387ab8c3e926244a15
StealWonders <6114858+StealWonders@users.noreply.github.com>
2023-06-12 14:19:27 +0200

    fix: Install required dependencies and make pipeline pass

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd data || exit  # exit if data directory does not exist
+#cd data || exit  # exit if data directory does not exist
 
 #python pull-data.py
 #pytest tests/test_pipeline.py
